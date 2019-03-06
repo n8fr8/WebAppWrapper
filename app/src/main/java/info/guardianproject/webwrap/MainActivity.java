ce12a2e31786409cec00a641038a62f7aa71e833
n8fr8 <nathan@guardianproject.info>
2019-03-06 09:01:32 -0500

    improve performance, set default site to GDrive mobile edition

--- app/src/main/java/info/guardianproject/webwrap/MainActivity.java
+++ app/src/main/java/info/guardianproject/webwrap/MainActivity.java
@@ -1,7 +1,10 @@
 package info.guardianproject.webwrap;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
         webView = (WebView) findViewById(R.id.webView);
 
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
 
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
@@ -31,8 +36,17 @@ public class MainActivity extends AppCompatActivity {
         webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         webView.setScrollbarFadingEnabled(false);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // chromium, enable hardware acceleration
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else {
+            // older android version, disable hardware acceleration
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
         String userAgent = webView.getSettings().getUserAgentString();
 
+	/**
         try {
             String ua = webView.getSettings().getUserAgentString();
             String androidOSString = webView.getSettings().getUserAgentString().substring(ua.indexOf("("), ua.indexOf(")") + 1);
@@ -42,11 +56,12 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+	**/
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                setTitle(view.getTitle());
+            //    setTitle(view.getTitle());
             }
         });
 
